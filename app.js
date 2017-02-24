1b1d361809d0bdc8b54ff76a712e115ee510381c
DESKTOP-NUN5RK4\김주형 <14wnrkim@gmail.com>
2017-02-24 22:13:16 +0900

    CORS OPTIONS Method 추가

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ import cors       from 'cors';
 const app     = express();
 const port    = process.env.PORT;
 const corsOptions = {
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
   exposedHeaders: "x-auth",
 };
 
@@ -21,13 +22,6 @@ app.use(cors(corsOptions));
 //Socket 테스트위해 임시허용
 const publicPath = path.join(__dirname, '../public');
 app.use(express.static(publicPath));
-
-// for cross domain origin problem
-// app.all('/', function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-//   next();
-//  });
 
 import Router from './router/index';
 Router(app);
@@ -44,29 +38,16 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'))
 });
 
-// app.get('/main1', (req, res) => {
-//   console.log('connected');
-//   res.sendFile(path.join(__dirname, 'public/socket/index1.html'))
-// });
-//
-// app.get('/main2', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public/socket/index2.html'))
-// });
-//
-// app.get('/sub1', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public/socket/sub_index.html'))
-// });
-//
-// app.get('/sub2', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public/socket/sub_index2.html'))
-// });
-//
-// app.get('/upload_img', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public/s3test.html'));
-// })
 
 server.listen(port, () => {
   console.log(`Connteced to port ${port}`);
+});
+
+// Graceful stop
+process.on('SIGINT', () => {
+  setTimeout(() => {
+    process.exit();
+  }, 300)
 });
 
 module.exports = {app, server};
